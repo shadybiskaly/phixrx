0167369bb4110685cc960a5607e226516c571809
shadybiskaly <shadybiskaly@gmail.com>
2026-07-27 14:32:33 -0400

    Add files via upload

--- build-css.py
+++ build-css.py
@@ -29,7 +29,7 @@ RGB = {
 }
 
 SPACE = {
-    "0": "0px", "1": "0.25rem", "1.5": "0.375rem", "2": "0.5rem", "2.5": "0.625rem",
+    "0": "0px", "0.5": "0.125rem", "1": "0.25rem", "1.5": "0.375rem", "2": "0.5rem", "2.5": "0.625rem",
     "3": "0.75rem", "3.5": "0.875rem", "4": "1rem", "5": "1.25rem", "6": "1.5rem",
     "7": "1.75rem", "8": "2rem", "9": "2.25rem", "10": "2.5rem", "11": "2.75rem",
     "12": "3rem", "14": "3.5rem", "16": "4rem", "20": "5rem", "24": "6rem",
@@ -291,6 +291,9 @@ def rule(cls):
     if c.startswith("right-"):
         v = SPACE.get(c[6:])
         if v: return f"right:{v}"
+    if c.startswith("bottom-"):
+        v = SPACE.get(c[7:])
+        if v: return f"bottom:{v}"
     if c == "overflow-hidden": return "overflow:hidden"
     if c == "mx-auto": return "margin-left:auto;margin-right:auto"
     if c == "pointer-events-none": return "pointer-events:none"
